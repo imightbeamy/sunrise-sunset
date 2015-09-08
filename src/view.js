d107d4f747502acb07e93aca148c8ab627ebbd0f
Amy Ciavolino <aciavolino@etsy.com>
2015-09-08 00:04:48 -0400

    Make links app friendly, add about and auto-refresh.

--- src/view.js
+++ src/view.js
@@ -1,6 +1,6 @@
 function tweetData(tweet) {
     return {
-        status_url: "https://twitter.com/statuses/" + tweet.tweet_id,
+        status_url: "https://twitter.com/" + tweet.handle + "/statuses/" + tweet.tweet_id,
         handle: tweet.handle,
         img_url: tweet.tweet_img_url,
         location: tweet.tweet_location,
