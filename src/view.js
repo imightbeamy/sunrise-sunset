5c720b1e5ee1b578f6a58ea8fe13f1a6adad81ce
Amy Ciavolino <aciavolino@etsy.com>
2015-09-07 20:47:00 -0400

    Fix image order

--- src/view.js
+++ src/view.js
@@ -2,7 +2,6 @@ function tweetData(tweet) {
     return {
         status_url: "https://twitter.com/statuses/" + tweet.tweet_id,
         handle: tweet.handle,
-        text: tweets.tweet_text,
         img_url: tweet.tweet_img_url
     };
 }
