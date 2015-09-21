3813a2fd3496175dd4e7c2cc5ba5d28f1b790bb7
Amy Ciavolino <aciavolino@etsy.com>
2015-09-20 20:56:23 -0400

    Split out mouse and touch interaction js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
-var current = more_suns.length - 1;
-var suns = document.querySelectorAll(".sun");
+var current = more_suns.length - 1,
+    suns = document.querySelectorAll(".sun"),
+    back = document.querySelector(".back"),
+    forward = document.querySelector(".forward");
 
 function setTweet(sun_image, tweet) {
     sun_image.style.backgroundImage = 'url("' + tweet.img_url + '")';
@@ -17,47 +19,58 @@ function updateImages(change) {
     }
 };
 
+var next = function() { updateImages(1); },
+    prev = function() { updateImages(-1); };
+
+function initTouch() {
+    // Buttons
+    forward.ontouchstart = next;
+    back.ontouchstart = prev;
+
+    // Mobile "scroll"
+    var touch_x;
+    document.ontouchstart = function(e) {
+        touch_x = e.changedTouches[0].clientX;
+    }
+    document.ontouchmove = function(e) {
+        var new_touch_x = e.changedTouches[0].clientX;
+        if (Math.abs(touch_x - new_touch_x) > 3) {
+            updateImages(new_touch_x > touch_x ? 1 : -1);
+            touch_x = new_touch_x;
+        }
+    }
+}
+
+function initMouse() {
+    // Buttons
+    forward.onclick = next;
+    back.onclick = prev;
+
+    // Scroll
+    window.onwheel = function(e) {
+        e.preventDefault();
+        if (e.deltaX) {
+            updateImages(e.deltaX > 0 ? 1 : -1);
+        }
+    }
+
+    // Arrow keys
+    document.onkeydown = function(e) {
+        switch(e.which) {
+            case 37: prev(); break; // left
+            case 39: next(); break; // right
+        }
+    };
+}
+
+// Add time to inital images (in the client timezone / format)
 updateImages(0);
 
-// Buttons
-document.querySelector(".back").onclick = function() {
-    updateImages(-1);
-};
-document.querySelector(".forward").onclick = function() {
-    updateImages(1);
-};
-
-// Desktop scroll
-window.onwheel = function(e) {
-    e.preventDefault();
-    if (e.deltaX) {
-        updateImages(e.deltaX > 0 ? 1 : -1);
-    }
-}
-
-// Arrow keys
-document.onkeydown = function(e) {
-    console.log(e.which);
-    switch(e.which) {
-        case 37: // left
-        updateImages(-1);
-        break;
-
-        case 39: // right
-        updateImages(1);
-        break;
-    }
-};
-
-// Mobile "scroll"
-var touch_x;
-document.ontouchstart = function(e) {
-    touch_x = e.changedTouches[0].clientX;
-}
-document.ontouchmove = function(e) {
-    var new_touch_x = e.changedTouches[0].clientX;
-    updateImages(new_touch_x > touch_x ? 1 : -1);
-    touch_x = new_touch_x;
+// Init interactions for phones or desktop/mouse
+if ('ontouchstart' in document.documentElement) {
+    initTouch();
+} else {
+    initMouse();
 }
 
 // Refresh every 15 minutes
